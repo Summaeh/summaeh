37f4da9e68f5fa883a4bbd7123c0a43dd86c25e1
Bernardo Henrique <bernardohrl.trabalho@gmail.com>
2017-04-26 16:10:46 -0300

    Starting videos details page

--- src/summaeh/config/urls.py
+++ src/summaeh/config/urls.py
@@ -11,6 +11,7 @@ from django.views import defaults as default_views
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='pages/home.html'), name='home'),
     url(r'^about/$', TemplateView.as_view(template_name='pages/about.html'), name='about'),
+    url(r'^video/1/', TemplateView.as_view(template_name='pages/video_details.html'), name='video_detail'),
 
     # Django Admin, use {% url 'admin:index' %}
     url(settings.ADMIN_URL, admin.site.urls),
