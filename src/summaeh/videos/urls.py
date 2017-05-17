37018b5d81aae23f4e66208668992cf25f04a0c9
Luis Henrique <luishng123@gmail.com>
2017-05-17 06:22:30 -0300

    Implemeting video detail

--- src/summaeh/videos/urls.py
+++ src/summaeh/videos/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'videos'
 urlpatterns = [
     url(r'^$', views.list_videos, name='list'),
-    url(r'^adicionar_video/', views.add_video, name='add'),
+    url(r'^adicionar_video/$', views.add_video, name='add'),
+    url(r'^detalhar_video/(?P<id_video>[0-9]+)/$', views.detail_video, name='detail'),
 ]
-
