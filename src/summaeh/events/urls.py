77f94300386cb9b69555e8e9811fc049e9a4f448
nicacioneto <nicacionetobsb@hotmail.com>
2017-06-10 16:10:09 -0300

    Adding list and detai event

--- src/summaeh/events/urls.py
+++ src/summaeh/events/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'events'
 urlpatterns = [
     url(r'^$', views.list_events, name='list'),
-    # url(r'^detalhar_evento/(?P<id_event>[0-9]+)/$', views.detail_event(), name='detail'),
+    url(r'^detalhar_evento/(?P<id_event>[0-9]+)/$', views.detail_event, name='detail'),
 ]
